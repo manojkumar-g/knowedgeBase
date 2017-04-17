dc91ec899a8da7dbff1e0a7bc7ba9e13387c3ca7
osgdev <osgdev@DHZ-MeanStk23.wipro.com>
2017-04-17 09:45:25 +0000

    adding comment publish

--- src/components/showStory.jsx
+++ src/components/showStory.jsx
@@ -6,7 +6,7 @@ import {toggleModal} from '../actions/auth.js'
 class Story extends React.Component{
   constructor(props) {
     super(props)
-    this.state = {post:{data:[]},write:false}
+    this.state = {post:{data:[]},write:false,comment:''}
   }
   componentDidMount(){
     if(this.props.params.id.length){
@@ -24,10 +24,23 @@ class Story extends React.Component{
         this.props.toggleModal()
     else{
       this.setState({write:true})
-      console.log(this.comment);
       this.comment.focus()
     }
 
+  }
+  onChange = ({target:{value}}) => {
+    this.setState({comment:value})
+  }
+  sendComment = () =>{
+    console.log({
+      author:this.props.email,
+      data:this.state.comment,
+      id:this.state.post._id
+    });
+    this.setState({
+      comment:'',
+      write:false
+    })
   }
   render(){
     return(
@@ -59,24 +72,43 @@ class Story extends React.Component{
                 }
               </div>
               <div className ='comtxt'>
-                <span onClick = {this.checkAuth}>Write your Comment ...</span>
+                <span onClick = {this.checkAuth}>
+                  {
+                    !this.state.write ?
+                    'Write your Comment ...':
+                    this.props.isLoggedIn ?
+                    <span style={{color:'green'}}>{this.props.name+''}</span>:
+                    'Write your Comment ...'
+                  }
+
+                </span>
               </div>
             </header>
             <div className ='txtara'>
-
-                <textarea className = '.txtara'
-                  style = {{
-                    height:this.state.write?'150px':'0px',
-                    opacity:this.state.write?1:0,
-                  }}
-                  placeholder ='Write your comment...'
-                  autofocus
-                  ref ={(inp) => {this.comment = this}}
-                />
-
+              {
+                this.props.isLoggedIn ?
+                <div className = 'txtaracont'>
+                  <textarea className = '.txtara'
+                    style = {{
+                      height:this.state.write?'150px':'0px',
+                      opacity:this.state.write?1:0,
+                    }}
+                    placeholder ='Write your comment...'
+                    ref ={(inp) => {this.comment = inp}}
+                    value ={this.state.comment}
+                    onChange = {this.onChange}
+                  />
+                  <button
+                    onClick = {this.sendComment}
+                    style = {{
+                      opacity:this.state.write?1:0,
+                    }}
+                    >Comment</button>
+              </div>
+                :''
+              }
             </div>
           </div>
-
         </article>
       </section>
     )
@@ -86,7 +118,7 @@ class Story extends React.Component{
 export default connect(
   ({userData}) => ({
     isLoggedIn:userData.isLoggedIn,
-    name:userData.name,
+    name:userData.firstName,
     email:userData.email
   }),
   {toggleModal}
